0151b3d3b3a37b90df5ab60f14d60458259e3184
Mikhail Lutsenko <m.g.lutsenko@gmail.com>
2025-11-06 17:57:45 +0300

    feat: install `jotai` and prepare for implementing state for `peopleId`

---
+++ src/atoms/peopleId.ts
@@ -0,0 +1,3 @@
+import { atom } from 'jotai';
+
+export const peopleId = atom<number | null>(null);
--- src/components/People/columns.tsx
+++ src/components/People/columns.tsx
@@ -1,10 +1,20 @@
 import { type TPeople } from '@/types';
 import { type ColumnDef } from '@tanstack/react-table';
 
+import { Button } from '@/components/ui';
+
 const columns: ColumnDef<TPeople>[] = [
   {
     accessorKey: 'name',
     header: 'Name'
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const people = row.original;
+
+      return <Button>{people.url}</Button>;
+    }
   }
 ];
 
